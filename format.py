2885ac22a2cde1078aba7fcd669b5744fa377a25
Samantha <samantha.goldstein31@gmail.com>
2015-07-18 10:49:08 -0400

    restructuring data and lib, removing vestigial files

--- format.py
+++ format.py
@@ -1,20 +1,16 @@
+"""
+Takes init data and parses to JSON with only relevant information including
+location, mass, year, longitude, and latitude of fallen meteorites
+"""
+
 import csv
 import json
 def main():
 
 
-    # with open('data.txt', 'w') as outfile:
-    #   json.dump(data, outfile)
-    # fl = open('meteoritessize.csv', 'r')
-    # output = open('output.csv', 'w')
 
-    # PARSED HEADER:
-    # location,mass,year,lat,long
-
-    fl = open('meteoritessize.csv', 'r')
-    # output = open('parsed_sample.json', 'w')
+    fl = open('data/meteoritessize.csv', 'r')
     reader = csv.reader(fl)
-    # writer = csv.writer(output)
     lns = []
 
     inum = 0
@@ -23,25 +19,21 @@ def main():
     for ln in reader:
       if (inum != 0):
         if (ln[3]=='Fell'):
-          #writer.writerow( [ln[0], int(ln[2].replace(',','')), ln[3], ln[4], ln[6], ln[7]] )
           new_ln = [ln[0], ln[2].replace(',',''), ln[4], ln[6], ln[7] ]
           if float(new_ln[1]) > 0:
               print new_ln
               node = { 'location': new_ln[0], 'mass': new_ln[1], 'year': new_ln[2], 'lat': new_ln[3], 'long': new_ln[4]}
               if d.has_key(new_ln[2]):
-                  d[new_ln[2]].append(node) #new_ln)
+                  d[new_ln[2]].append(node)
               else:
-                  d[new_ln[2]] = [node] #[new_ln]
-          #TODO: keep below line, move
-          #writer.writerow( [ln[0], ln[2].replace(',',''), ln[4], ln[6], ln[7]] )
+                  d[new_ln[2]] = [node] 
       inum +=1
 
 
 
-    with open('parsed_sample.json', 'w') as outfile:
+    with open('data/parsed_sample.json', 'w') as outfile:
       json.dump(d, outfile)
 
-    # lns.sort(key=lambda x:x[2])
 
 
 
